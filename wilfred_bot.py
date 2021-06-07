5738d29ad320210dc63564af2106f34476134a2a
Marco Vethanayagam <29697062+Macomatic@users.noreply.github.com>
2021-06-07 16:48:48 -0400

    Created individual commands for each weather condition

--- wilfred_bot.py
+++ wilfred_bot.py
@@ -22,49 +22,90 @@ async def test(ctx,*arg):
     await ctx.send('{}'.format(" ".join(arg)))
 
 @bot.command(pass_context=True)
-async def weather(ctx, location):
+async def temperature(ctx, location):
+
+    # Init
     mgr = owm.weather_manager()
     weatherInfo = mgr.weather_at_place(location).weather # Stores all weather info at that location
     weatherInfoList = [] # Holds all specific weather information
-    lastHour = False # Need to implement rain command to take in param to indicate 1h or 3h
-
+    
     # Temperature
-    weatherInfoList.append("Average Temperature: " + str(round(weatherInfo.temperature()['temp'] -273.15,1)) + " °C") # Add current temperature in °C
-    weatherInfoList.append("Minimum Temperature: " + str(round(weatherInfo.temperature()['temp_min'] -273.15,1)) + " °C")
-    weatherInfoList.append("Maximum Temperature: " + str(round(weatherInfo.temperature()['temp_max'] -273.15,1)) + " °C")
+    weatherInfoList.append("__Temperature__")
+    weatherInfoList.append("**Average Temperature: **" + str(round(weatherInfo.temperature()['temp'] -273.15,1)) + " °C") 
+    weatherInfoList.append("**Minimum Temperature: **" + str(round(weatherInfo.temperature()['temp_min'] -273.15,1)) + " °C")
+    weatherInfoList.append("**Maximum Temperature: **" + str(round(weatherInfo.temperature()['temp_max'] -273.15,1)) + " °C")
+    weatherInfoList.append("**Current Weather Status: **" + str(weatherInfo.detailed_status).title())
+    weatherInfoList.append("**Humidity: **" + str(weatherInfo.humidity)+ "%") # Add current humidity in %
+
+
+    # Output
+    for i in range(len(weatherInfoList)): # Sends all information stored in weatherInfoList
+        await ctx.send(weatherInfoList[i])
     
+@bot.command(pass_context=True)
+async def wind(ctx, location):
+
+    # Init
+    mgr = owm.weather_manager()
+    weatherInfo = mgr.weather_at_place(location).weather # Stores all weather info at that location
+    weatherInfoList = [] # Holds all specific weather information
+
     # Wind
-    weatherInfoList.append("Wind Speed: " + str(weatherInfo.wind()['speed']) + " m/s")
+    weatherInfoList.append("__Wind__")
+    weatherInfoList.append("**Wind Speed: **" + str(weatherInfo.wind()['speed']) + " m/s")
 
-    # Rainfall
-    rain_dict = mgr.weather_at_place(location).weather.rain
-    if lastHour:
-        if '1h' in rain_dict:
-            weatherInfoList.append("Rain in the last hour: " + str(rain_dict['1h']) + " mm")
-        
-        else:
-            weatherInfoList.append("Rain in the last hour: 0.0 mm")
-
-    if not lastHour:
-        if '3h' in rain_dict:
-            weatherInfoList.append("Rain in the last 3 hours: " + str(rain_dict['3h']) + " mm")
-        
-        else:
-            weatherInfoList.append("Rain in the last 3 hours: 0.0 mm")
-
-    # Sunrise/Sunset
-    sunrise = weatherInfo.sunrise_time() - 14400 # -14400 for GMT to EST offset; work on a general solution
-    sunset = weatherInfo.sunset_time() - 14400
-
-    weatherInfoList.append("Time of Sunrise: " + datetime.utcfromtimestamp(sunrise).strftime('%Y-%m-%d %H:%M:%S') + " EST")
-    weatherInfoList.append("Time of Sunset: " + datetime.utcfromtimestamp(sunset).strftime('%Y-%m-%d %H:%M:%S') + " EST")
-
-    weatherInfoList.append("Humidity: " + str(weatherInfo.humidity)+ "%") # Add current humidity in %
-    weatherInfoList.append("Current Weather Status: " + str(weatherInfo.detailed_status).title())
-    
+    # Output
     for i in range(len(weatherInfoList)): # Sends all information stored in weatherInfoList
         await ctx.send(weatherInfoList[i])
 
+@bot.command(pass_context=True)
+async def rainfall(ctx,location,timeframe):
 
+    # Init
+    mgr = owm.weather_manager()
+    weatherInfo = mgr.weather_at_place(location).weather # Stores all weather info at that location
+    weatherInfoList = [] # Holds all specific weather information
+    lastHour = (timeframe == '1h') # Need to implement rain command to take in param to indicate 1h or 3h
+
+    # Rainfall
+    weatherInfoList.append("__Rainfall__")
+    rain_dict = mgr.weather_at_place(location).weather.rain
+    if lastHour:
+        if '1h' in rain_dict:
+            weatherInfoList.append("**Rain in the last hour: **" + str(rain_dict['1h']) + " mm")
+        
+        else:
+            weatherInfoList.append("**Rain in the last hour:** 0.0 mm")
+
+    if not lastHour:
+        if '3h' in rain_dict:
+            weatherInfoList.append("**Rain in the last 3 hours:** " + str(rain_dict['3h']) + " mm")
+        
+        else:
+            weatherInfoList.append("**Rain in the last 3 hours:** 0.0 mm")
+
+    # Output
+    for i in range(len(weatherInfoList)): # Sends all information stored in weatherInfoList
+        await ctx.send(weatherInfoList[i])
+
+@bot.command(pass_context=True)
+async def sun(ctx, location):
+
+    # Init
+    mgr = owm.weather_manager()
+    weatherInfo = mgr.weather_at_place(location).weather # Stores all weather info at that location
+    weatherInfoList = [] # Holds all specific weather information
+
+    # Sunrise/Sunset
+    weatherInfoList.append("__Sunrise and Sunset__")
+    sunrise = weatherInfo.sunrise_time() - 14400 # -14400 for GMT to EST offset; work on a general solution
+    sunset = weatherInfo.sunset_time() - 14400
+
+    weatherInfoList.append("**Time of Sunrise: **" + datetime.utcfromtimestamp(sunrise).strftime('%H:%M:%S') + " EST")
+    weatherInfoList.append("**Time of Sunset: **" + datetime.utcfromtimestamp(sunset).strftime('%H:%M:%S') + " EST")
+
+    # Output
+    for i in range(len(weatherInfoList)): # Sends all information stored in weatherInfoList
+        await ctx.send(weatherInfoList[i])
 
 bot.run("NzY4MTc2MjM5NjkyMjE4NDU5.X48p3w.BmgJCOwqboe8ao7zF1GK3DYSUeo")
