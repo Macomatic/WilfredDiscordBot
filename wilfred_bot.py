2749ac5dbd2cf0b5f06242a3bab5cac1404d28cb
Marco Vethanayagam <29697062+Macomatic@users.noreply.github.com>
2021-06-07 13:09:22 -0400

    Added command expandability

--- wilfred_bot.py
+++ wilfred_bot.py
@@ -1,19 +1,21 @@
 import discord
-from profanity_filter import ProfanityFilter
+from discord.ext import commands, tasks
+from discord.ext.commands import Bot
 
-#pf = ProfanityFilter()
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+print("Initializing Wilfred Bot...")
+
+@bot.event
 async def on_ready():
-    print("Bot online")
+    #Prints to console when the bot is ready/runs
+    print(bot.user.name)
+    print(bot.user.id)
+    print("Online")
 
-@client.event
-async def on_message(message):
-    if message.author != client.user:
-        if message.content.startswith("!Hi"):
-            await message.channel.send("Hello user!")
-        #if message.content:
-            #await message.channel.send(pf.censor(message.content))
+@bot.command()
+async def test(ctx,*arg):
+    await ctx.send('{}'.format(" ".join(arg)))
 
-client.run("NzY4MTc2MjM5NjkyMjE4NDU5.X48p3w.BmgJCOwqboe8ao7zF1GK3DYSUeo")
+
+bot.run("NzY4MTc2MjM5NjkyMjE4NDU5.X48p3w.BmgJCOwqboe8ao7zF1GK3DYSUeo")
